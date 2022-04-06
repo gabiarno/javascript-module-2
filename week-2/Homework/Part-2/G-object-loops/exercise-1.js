062860b4cb95aafa9f8d8ee3e824f952e315d4d5
gabiarno <gabiarno@hotmail.com>
2022-04-06 22:20:30 +0200

    homework week 2

--- week-2/Homework/Part-2/G-object-loops/exercise-1.js
+++ week-2/Homework/Part-2/G-object-loops/exercise-1.js
@@ -3,11 +3,29 @@
 // Try to use both methods shown above to achieve this
 
 const studentGrades = {
-    tom: 20,
-    george: 17,
-    abdul: 19,
-  };
-  
-  // Prints
-  // TOM - 20
-  // ABDUL - 19
+  tom: 20,
+  george: 17,
+  abdul: 19,
+};
+
+// Prints
+// TOM - 20
+// ABDUL - 19
+
+const students = Object.keys(studentGrades);
+const grades = Object.values(studentGrades);
+const studentGradesList = Object.entries(studentGrades);
+console.log("studentGrades", studentGrades);
+
+function moreThan18(num) {
+  return num > 18;
+}
+var moreThan18List = (list) => {
+  return list.filter((element) => moreThan18(element[1]));
+};
+
+const result = moreThan18List(studentGradesList);
+
+for (let i = 0; i < result.length; i++) {
+  console.log(`${result[i][0]} - ${result[i][1]}`);
+}
